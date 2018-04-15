4569d400454c212d221351e4248cc96eeea954f1
priyam1011 <patelpriyam10@yahoo.com>
2018-04-14 20:44:08 -0400

    first commit

---
+++ Assets/Tutorial Assets/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.transform.position.y < -7) {
+			Die ();
+		}
+	}
+
+	void Die() {
+		SceneManager.LoadScene ("Scene 1");
+	}
+}
